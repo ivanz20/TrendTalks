8d522ef528916e8885999e72918f611c626ef234
ivanz <ivanzavalamontes20@hotmail.com>
2023-05-27 08:16:30 -0600

    Populares

--- src/routes/post.js
+++ src/routes/post.js
@@ -51,6 +51,55 @@ router.get("/talkie/:username", (req, res) => {
         })
 });
 
+//Get Posts by Likes
+router.get("/talkie/count/likes", (req, res) => {
+    PostSchema
+        .find().sort({likes:-1})
+        .then((data) => res.json(data))
+        .catch((error) => res.json({ message: error }))
+});
+
+
+//Get Posts by Hashs
+router.get("/talkie/count/hash", (req, res) => {
+    PostSchema
+    .find({}, (erro, data) => {
+
+        if (erro) {
+            return res.status(500).json({
+                ok: false,
+                err: erro
+            })
+        }
+        if (data.length > 0) {
+            var auxarray = []
+            for (var x in data){
+                var aux = data[x]["hashs"];
+                aux = aux.split("#")
+                var auxarray = data[x];
+                var auxilio = JSON.stringify(aux);
+                auxarray[x] = data[x]
+                auxarray[x]["hashs"] = auxilio;
+                // auxarray[x]["counthash"] = aux.length()-1;
+
+            }
+            var json2 = JSON.stringify(auxarray);
+            return res.status(500).json({
+                ok: false,
+                posts: json2
+            })
+        } else {
+            return res.status(400).json({
+                ok: false,
+                err: {
+                    message: "El usuario no tiene posts"
+                }
+            })
+        }
+    })
+});
+
+
 //Get Post by id
 router.get("/talkie/post/:id", (req, res) => {
     const { id } = req.params;
